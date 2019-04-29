9b754de3c100111197eb25c7be53e5172278098c
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2019-04-29 20:44:24 +0900

    Chapter 10 done

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,68 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "unsuccessfull edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template "users/edit"
+    patch user_path(@user), params: {
+      user: {
+        name: "",
+        email: "invalid@mail",
+        password: "foo",
+        password_confirmation: "bar"
+      }
+    }
+
+    assert_template 'users/edit'
+    assert_select "div.alert", count: 1
+  end
+
+  test "successfull edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template "users/edit"
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: {
+      user: {
+        name: name,
+        email: email,
+        password: "",
+        password_confirmation: ""
+      }
+    }
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+
+  test "successfull edit with friendly forwardig" do
+    get edit_user_path(@user)
+    log_in_as @user
+    assert_redirected_to edit_user_path(@user)
+
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: {
+      user: {
+        name: name,
+        email: email,
+        password: "",
+        password_confirmation: ""
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+end
